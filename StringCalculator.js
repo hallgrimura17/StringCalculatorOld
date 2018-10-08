1d100fedc1843053d430a243563585b4269180ec
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-08 17:02:47 +0000

    implemented for a single number

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,6 +1,10 @@
 function add(numbers)
 {
-    return 0;
+    if(numbers == "")
+    {
+        return 0;
+    }
+    return parseInt(numbers);
 }
 
 module.exports = add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -3,3 +3,7 @@ const add = require("./StringCalculator");
 it("should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+it("should return the number when only a number is in the string", () => {
+    expect(add("4")).toBe(4);
+});
